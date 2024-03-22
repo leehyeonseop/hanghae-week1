c136f517a17d6099efb7df3439a3cb1e9645e957
leehyeonseop <gustjq4146@naver.com>
2024-03-22 22:58:11 +0900

    2주차: 나만의 React 만들기

--- packages/chapter2/src/MyReact.js
+++ packages/chapter2/src/MyReact.js
@@ -1,11 +1,35 @@
 import { createHooks } from "./hooks";
-import { render as updateElement } from "./render";
+import { createElement, render as updateElement } from "./render";
 
 function MyReact() {
-  const _render = () => {};
-  function render($root, rootComponent) {}
+  let root;
+  let prevDOM;
+  let jsx;
 
-  const { useState, useMemo, resetContext: resetHookContext } = createHooks(_render);
+  const _render = () => {
+    resetHookContext();
+    const currentDOM = jsx();
+
+    updateElement(root, currentDOM, prevDOM);
+    prevDOM = currentDOM;
+  };
+
+  function render($root, rootComponent) {
+    resetHookContext();
+
+    root = $root;
+    // 렌더링 하는 순간 바로 이전 DOM이 됨.
+    prevDOM = rootComponent();
+    jsx = rootComponent;
+
+    updateElement($root, prevDOM);
+  }
+
+  const {
+    useState,
+    useMemo,
+    resetContext: resetHookContext,
+  } = createHooks(_render);
 
   return { render, useState, useMemo };
 }
--- packages/chapter2/src/hooks.js
+++ packages/chapter2/src/hooks.js
@@ -1,15 +1,39 @@
 export function createHooks(callback) {
+  const global = { states: [], memoizedValues: [] };
+  let index = 0;
+
   const useState = (initState) => {
-    return [];
+    const currentState = global.states[index] || initState;
+    global.states[index] = currentState;
+
+    const setState = ((currentIndex) => {
+      return function (value) {
+        if (global.states[currentIndex] === value) return;
+        global.states[currentIndex] = value;
+        callback();
+      };
+    })(index);
+
+    index = index + 1;
+    return [currentState, setState];
   };
 
   const useMemo = (fn, refs) => {
-    return fn();
+    const memoRecord = global.memoizedValues[index];
+
+    if (memoRecord && refs.every((ref, i) => ref === memoRecord.refs[i])) {
+      return memoRecord.value;
+    } else {
+      const newValue = fn();
+      global.memoizedValues[index] = { value: newValue, refs };
+      index++;
+      return newValue;
+    }
   };
 
   const resetContext = () => {
-
-  }
+    index = 0; // 상태 인덱스를 초기화
+  };
 
   return { useState, useMemo, resetContext };
 }
